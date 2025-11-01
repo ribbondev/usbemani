cbba641abda6f31b8fa8a0ee39861ffd969ced5d
lain-2p <lain.md@icloud.com>
2025-11-01 14:53:21 +0000

    correct button inversions for gen1 mini-4 optical

--- controllers/lain/mini-4/config.h
+++ controllers/lain/mini-4/config.h
@@ -7,8 +7,8 @@
 #define BUTTONS_ACTIVE  11
 #define BUTTON_INVERTED 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1
 
-/*** Uncomment for Mini-4 with conventional E1-E4 switches ***/
-//#define BUTTON_INVERTED 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1
+/*** Uncomment for Mini-4 with optical 1-7 switches and conventional E1-E4 switches ***/
+//#define BUTTON_INVERTED 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0
 
 
 //Button keyboard definitions represent BUTTONS_ACTIVE plus 2 per turntable, 15 total for an 11-key keypad with two turntables. See scan code definitions in /common/button/defs.h
