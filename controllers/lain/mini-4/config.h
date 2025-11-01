6f3d64dd3fdbf9a320a00d55057e043f4cdec324
lain-2p <lain.md@icloud.com>
2025-11-01 14:43:24 +0000

    Add button inverting to mini-4 controller config

--- controllers/lain/mini-4/config.h
+++ controllers/lain/mini-4/config.h
@@ -5,6 +5,11 @@
 
 /*** Buttons ***/
 #define BUTTONS_ACTIVE  11
+#define BUTTON_INVERTED 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1
+
+/*** Uncomment for Mini-4 with conventional E1-E4 switches ***/
+//#define BUTTON_INVERTED 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1
+
 
 //Button keyboard definitions represent BUTTONS_ACTIVE plus 2 per turntable, 15 total for an 11-key keypad with two turntables. See scan code definitions in /common/button/defs.h
 #define BUTTONS_KEYBOARD_DEFINITION {KEY_A, KEY_S, KEY_D, KEY_F, KEY_G, KEY_H, KEY_J, KEY_ENTER, KEY_BACKSPACE, KEY_PAGEUP, KEY_PAGEDOWN, KEY_UP, KEY_DOWN, KEY_LEFT, KEY_RIGHT} 
